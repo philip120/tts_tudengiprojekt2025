82a2944732b2cf7f2ae3a4298dfc6f05a2a2f437
OskarMannik <mannikoskar@gmail.com>
2025-05-04 17:33:29 +0300

    vue fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  root: '.', // Set the root directory to the frontend folder
+  root: '.',
+  build: {
+    outDir: 'dist', // Output directory for build files
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'), // Ensure index.html is included in the build
+      },
+    },
+  },
 })
